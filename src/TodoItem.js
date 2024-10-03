6760e62378f6aeebcd0e36323c942322d0ce03cc
Brayan Bautista <brayan.bautista@ibit.pe>
2024-10-03 15:52:38 -0500

    10-icons

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,20 +1,21 @@
 import "./TodoItem.css";
+import { AiOutlineCheckCircle } from "react-icons/ai";
+import { AiOutlineCloseCircle } from "react-icons/ai";
 
 function TodoItem(props) {
   return (
     <li className="TodoItem">
-      <span
+      <AiOutlineCheckCircle
         className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}
         onClick={props.onComplete}
-      >
-        V
-      </span>
+      />
       <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>
         {props.text}
       </p>
-      <span className="Icon Icon-delete" onClick={props.onDelete}>
-        X
-      </span>
+      <AiOutlineCloseCircle
+        className="Icon Icon-delete"
+        onClick={props.onDelete}
+      />
     </li>
   );
 }
