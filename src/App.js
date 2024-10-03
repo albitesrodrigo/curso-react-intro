6fbb7b6c0e393f51aabdca81208495ee0158c838
Brayan Bautista <brayan.bautista@ibit.pe>
2024-10-03 16:39:43 -0500

    11-local-storage

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ function App() {
   const completeTodo = (text) => {
     const newTodos = [...todos];
     const todoIndex = newTodos.findIndex(
-      (todo) => todo.text == text
+      (todo) => todo.text === text
     );
     newTodos[todoIndex].completed = true;
     saveTodos(newTodos);
@@ -62,7 +62,7 @@ function App() {
   const deleteTodo = (text) => {
     const newTodos = [...todos];
     const todoIndex = newTodos.findIndex(
-      (todo) => todo.text == text
+      (todo) => todo.text === text
     );
     newTodos.splice(todoIndex, 1);
     saveTodos(newTodos);
