c9b8813dbdbbb1e37a976e36af06e23516d7a9a0
Brayan Bautista <brayan.bautista@ibit.pe>
2024-10-03 13:27:31 -0500

    06-estado

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,15 +1,17 @@
+import React, { useState } from "react";
 import "./TodoSearch.css";
 
 function TodoSearch() {
+  const [searchValue, setSearchValue] = useState("");
+
+  console.log('Los usuarios buscan todos de ' + searchValue);
+
   return (
     <input
       className="TodoSearch"
       placeholder="Cebolla"
-      onChange={(event) => {
-        console.log(event);
-        console.log(event.target);
-        console.log(event.target.value);
-      }}
+      value={searchValue}
+      onChange={(event) => setSearchValue(event.target.value)}
     />
   );
 }
