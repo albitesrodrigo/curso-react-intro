f83f9472bf1db0428470383be1293fa7b4fcd919
Brayan Bautista <brayan.bautista@ibit.pe>
2024-10-03 13:02:49 -0500

    05-eventos

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,7 +1,17 @@
 import "./TodoSearch.css";
 
 function TodoSearch() {
-  return <input className="TodoSearch" placeholder="Cebolla" />;
+  return (
+    <input
+      className="TodoSearch"
+      placeholder="Cebolla"
+      onChange={(event) => {
+        console.log(event);
+        console.log(event.target);
+        console.log(event.target.value);
+      }}
+    />
+  );
 }
 
 export { TodoSearch };
